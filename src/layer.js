d501ae83266b74b4e397c97caff7e02d719dfe8b
Yuki Kodama <endflow.net@gmail.com>
2016-02-19 17:32:04 +0900

    Hide dragging preview

--- src/layer.js
+++ src/layer.js
@@ -18,6 +18,7 @@ const boxSource = {
 
 @DragSource(LAYER, boxSource, (connect, monitor) => ({
   connectDragSource: connect.dragSource(),
+  connectDragPreview: connect.dragPreview(),
   isDragging: monitor.isDragging(),
 }))
 export default class Layer extends Component {
@@ -26,13 +27,25 @@ export default class Layer extends Component {
     offset: PropTypes.object,
     children: PropTypes.any.isRequired,
     connectDragSource: PropTypes.func.isRequired,
+    connectDragPreview: PropTypes.func.isRequired,
   };
   static defaultProps = {
     offset: { x: 0, y: 0 },
   };
 
+  componentDidMount() {
+    const { connectDragPreview } = this.props;
+    const img = new Image();
+    img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==';
+    img.onload = () => connectDragPreview(img);
+  }
+
   render() {
-    const { offset: { x, y }, connectDragSource, children } = this.props;
+    const {
+      offset: { x, y },
+      connectDragSource, children
+    } = this.props;
+
     return connectDragSource(
       <div style={{ position: 'absolute', left: `${x}px`, top: `${y}px` }}>
         {children}
